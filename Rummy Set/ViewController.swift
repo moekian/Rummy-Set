b9abd41d226d5559a13b90f26bbd289066838120
Mohammad Kiani <kianimohammad@outlook.com>
2021-09-12 19:08:39 -0400

    Initial Commit

---
+++ Rummy Set/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rummy Set
+//
+//  Created by Mohammad Kiani on 2021-09-12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
